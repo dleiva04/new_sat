f3c63177b3af9a7ce52a560e6b04d7a50e378c7d
dleiva04 <dleiva04>
2024-09-23 17:51:10 +0000

    update

--- notebooks/setup/list_workspaces.py
+++ notebooks/setup/list_workspaces.py
@@ -1,1 +1,31 @@
 # Databricks notebook source
+from databricks.sdk import WorkspaceClient, AccountClient
+
+# COMMAND ----------
+
+# MAGIC %run "../utils/sat_variables"
+
+# COMMAND ----------
+
+account = AccountClient(
+    host=sat_vars["account_url"],
+    account_id=sat_vars["account_id"],
+    client_id=sat_vars["client_id"],
+    client_secret=sat_vars["client_secret"],
+)
+
+# COMMAND ----------
+
+for w in account.workspaces.list():
+    if w.workspace_status_message == 'Workspace is running.':
+        try:
+            workspace = WorkspaceClient(
+                host=f'{w.deployment_name}{sat_vars["workspace_baseUrl"]}',
+                client_id=sat_vars["client_id"],
+                client_secret=sat_vars["client_secret"],
+            )
+            workspace.get_workspace_id()
+            print(w.workspace_name)
+            print("Valid")
+        except Exception as e:
+            continue
--- notebooks/utils/sat_variables.py
+++ notebooks/utils/sat_variables.py
@@ -6,10 +6,13 @@ import json
 sat_vars = {
     "verbosity": "info",
     "warehouse_id": "",
-    "catalog_name": "new_sat_david",
+    "catalog_name": "sat_david",
     "schema_name": "sat",
     "proxies": {},
-    "client_id": "",
+    "account_url": "https://accounts.cloud.databricks.com",
+    "workspace_baseUrl": ".cloud.databricks.com",
+    "account_id": "0d26daa6-5e44-4c97-a497-ef015f91254a",
+    "client_id": "34449fd8-e99d-4e9b-934c-0aa99d9e08b1",
     "client_secret": "",
 }
 sat_vars["database"] = f"{sat_vars['catalog_name']}.{sat_vars['schema_name']}"
